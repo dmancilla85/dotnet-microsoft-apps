e4ecfd6c1087ba7e412a704b89d399d5de4505e9
David A. Mancilla <david.a.m@live.com>
2022-07-28 23:06:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Microsoft Development Tools Collection
+
+This project is intended to be a collection of simple apps with different Microsoft Framework
+
+## 1. Microsoft Graph
+
+## 2. Microsoft Teams Application
+
+## 3. Office OneNote Addin
+
+## 4. Xamarin
---
+++ TeamsChatbot/TeamsBot.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder;
+
+namespace TeamsChatbot
+{
+  /// <summary>
+  /// An empty bot handler.
+  /// You can add your customization code here to extend your bot logic if needed.
+  /// </summary>
+  public class TeamsBot : IBot
+  {
+    public Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+    {
+      Console.WriteLine($"I receive a message: {turnContext.Activity.CallerId}");
+      return Task.CompletedTask;
+    }
+  }
+}
